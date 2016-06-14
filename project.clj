d253a825b1e1bf7c2a3bfc20cba0879d0879cd78
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2016-06-13 22:35:08 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cljsee "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cljsee "0.1.0"
+  :description "Backporting cljc"
+  :url "http://github.com/aengelberg/cljsee"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
